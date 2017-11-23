a310f6f700a9b221dea958f84b0bb5e4c54ab311
ParadoxOrigins <funxgame1@gmail.com>
2017-11-23 23:37:07 +0100

    Initial release: 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,90 @@
-# FelixBot-wrapper
-A wrapper for Felix's API
+<h1 align="center"> FelixWrapper </h1>
+  <p align="center">
+    <a href="https://david-dm.org/ParadoxalCorp/FelixBot-wrapper" target="_blank"><img src="https://david-dm.org/ParadoxalCorp/FelixBot-wrapper/status.svg" alt="Dependencies"></a>
+    <a href="https://github.com/ParadoxalCorp/FelixBot-wrapper/blob/master" target="_blank"><img src="https://img.shields.io/github/stars/ParadoxalCorp/FelixBot-wrapper.svg?style=social&label=Star" alt="Github Stars"></a>
+    <a href="https://github.com/ParadoxalCorp/FelixBot-wrapper/issues" target="_blank"><img src="https://img.shields.io/github/issues/ParadoxalCorp/FelixBot-wrapper.svg" alt="Github Issues"></a>
+  </p>
+
+  This is a wrapper for the API of Felix, a Discord bot
+
+  This wrapper covers 100% of Felix's API and will be updated at the same time as Felix's API is
+
+  All methods are async and returns promise, errors are in most case rejected
+
+### Constructor
+
+```js
+const FelixWrapper = require("felix-wrapper");
+
+let wrapper = new FelixWrapper({
+    url: "",
+    token: "",
+    timeout: 42
+});
+```
+
+  | Property | Type | Description |
+| --- | --- | --- |
+| url | <code>String</code> | Required. The base url of the domain to request to |
+| token | <code>String</code> | Optional. The token (private or public), optional but required for almost all endpoints |
+| timeout | <code>Number</code> | Optional. The time in ms before requests should be aborted, default is 3000
+
+### wrapper.status()
+
+**Returns**: A boolean based on whether Felix's API is up or not
+
+### wrapper.getUserData(user, options)
+
+**Returns**: The user object/array of user objects 
+
+  | Params | Type | Description |
+| --- | --- | --- |
+| user | <code>String || Array</code> | Required. The user ID or an array of IDs of the user(s) to fetch |
+| options | <code>Object</code> | Optional. An object of options |
+| options.timeout | <code>Number</code> | Optional. The time in ms before this request should be aborted, default is wrapper.timeout |
+
+### wrapper.postUserData(user, options)
+
+**Restricted to private tokens**
+
+**Returns**: The posted user object if the operation is successful, otherwise reject an error
+
+
+  | Params | Type | Description |
+| --- | --- | --- |
+| user | <code>String || Array</code> | Required. The user object to POST, will update or create a new database entry |
+| options | <code>Object</code> | Optional. An object of options |
+| options.timeout | <code>Number</code> | Optional. The time in ms before this request should be aborted, default is wrapper.timeout |
+
+### wrapper.getGuildData(guild, options)
+
+**Returns**: The guild object/array of guild objects 
+
+  | Params | Type | Description |
+| --- | --- | --- |
+| guild | <code>String || Array</code> | Required. The guild ID or an array of IDs of the guild(s) to fetch |
+| options | <code>Object</code> | Optional. An object of options |
+| options.timeout | <code>Number</code> | Optional. The time in ms before this request should be aborted, default is wrapper.timeout |
+
+### wrapper.postGuildData(guild, options)
+
+**Restricted to private tokens**
+
+**Returns**: The posted guild object if the operation is successful, otherwise reject an error
+
+
+  | Params | Type | Description |
+| --- | --- | --- |
+| guild | <code>String || Array</code> | Required. The guild object to POST, will update or create a new database entry |
+| options | <code>Object</code> | Optional. An object of options |
+| options.timeout | <code>Number</code> | Optional. The time in ms before this request should be aborted, default is wrapper.timeout |
+
+### wrapper.fetchClientValue(value)
+
+**Restricted to private tokens**
+
+**Returns**: The value
+
+  | Params | Type | Description |
+| --- | --- | --- |
+| value | <code>String</code> | Required. The value to fetch, this will be interpreted as a property name of the client object |
---
+++ index.js
@@ -0,0 +1,175 @@
+const Collection = require("enmap");
+const unirest = require("unirest");
+
+class FelixWrapper {
+    constructor(options) {
+        this.url = `${options.url}/api`;
+        this.token = `Bearer ${options.token}`;
+        this.timeout = options.timeout || 3000;
+    }
+
+    /**
+     * Returns a boolean of whether Felix's API is up or down
+     * @param {Object} [options] An optional object of options
+     * @param {number} [options.timeout=this.timeout] Time in milliseconds before the request should abort, default is the timeout set in the constructor or 3000
+     * @returns {Promise<boolean>}
+     */
+    status(options = {}) {
+        const wrapper = this;
+        return new Promise(async(resolve, reject) => {
+            unirest.get(`${wrapper.url}`)
+                .timeout(options.timeout ? options.timeout : wrapper.timeout)
+                .end(response => {
+                    resolve(response.statusCode === 200 ? true : false);
+                });
+        });
+    }
+
+    /**
+     * Get one or multiple users from the database
+     * @param {String||Array} user A user ID or an array of IDs, if none provided, will return a collection of all users
+     * @param {Object} [options] An optional object of options
+     * @param {number} [options.timeout=this.timeout] Time in milliseconds before the request should abort, default is the timeout set in the constructor or 3000
+     * @returns {Promise<User||Collection<ID, User>>}
+     */
+    getUser(user, options = {}) {
+        const wrapper = this;
+        return new Promise(async(resolve, reject) => {
+            unirest.get(`${wrapper.url}/getUserData/${user}`)
+                .header(`Authorization`, wrapper.token)
+                .timeout(options.timeout ? options.timeout : wrapper.timeout)
+                .end(response => {
+                    if (typeof response.error === "object" && response.error.Error === "ETIMEDOUT") return reject("timeout");
+                    if (response.statusCode !== 200) return reject(response.body);
+                    if (Array.isArray(response.body)) {
+                        let users = new Collection();
+                        response.body.forEach(u => { users.set(u.id, u) });
+                        return resolve(users);
+                    }
+                    if (!response.body.mutualGuilds) return resolve(response.body);
+                    let guilds = new Collection();
+                    //console.dir(response.body.mutualGuilds);
+                    response.body.mutualGuilds.forEach(guild => {
+                        let channels = new Collection(),
+                            roles = new Collection(),
+                            members = new Collection(),
+                            userPermissions = new Collection();
+                        guild.channels.forEach(c => {
+                            channels.set(c.id, c);
+                        });
+                        guild.roles.forEach(r => {
+                            roles.set(r.id, r);
+                        });
+                        guild.members.forEach(m => {
+                            members.set(m.id, m);
+                        });
+                        guild.userPermissions.map(c => {
+                            userPermissions.set(c.name, c.allowed);
+                        });
+                        guild.channels = channels,
+                            guild.roles = roles,
+                            guild.members = members,
+                            guild.userPermissions = userPermissions;
+                        guilds.set(guild.id, guild);
+                    });
+                    response.body.mutualGuilds = guilds;
+                    resolve(response.body);
+                });
+        });
+    }
+
+    /**
+     * Get one or multiple guilds from the database
+     * @param {String||Array} guilds A guild ID or an array of IDs, if none provided, will return a collection of all guilds
+     * @param {Object} [options] An optional object of options
+     * @param {number} [options.timeout=this.timeout] Time in milliseconds before the request should abort, default is the timeout set in the constructor or 3000
+     * @returns {Promise<Guild||Collection<ID, Guild>>}
+     */
+    getGuild(guild, options = {}) {
+        const wrapper = this;
+        return new Promise(async(resolve, reject) => {
+            unirest.get(`${wrapper.url}/getGuildData/${guild}`)
+                .header(`Authorization`, wrapper.token)
+                .timeout(options.timeout ? options.timeout : wrapper.timeout)
+                .end(response => {
+                    if (typeof response.error === "object" && response.error.Error === "ETIMEDOUT") return reject("timeout");
+                    if (response.statusCode !== 200) return reject(response.body);
+                    if (Array.isArray(response.body)) {
+                        let guilds = new Collection();
+                        response.body.forEach(u => { guilds.set(g.id, g) });
+                        return resolve(guilds);
+                    }
+                    resolve(response.body);
+                });
+        });
+    }
+
+    /**
+     * Fetch a value of Felix, this is restricted to private tokens only 
+     * @param {String} value The value to fetch, this will be interpreted as the name of the property
+     * @param {Object} [options] An optional object of options
+     * @param {number} [options.timeout=this.timeout] Time in milliseconds before the request should abort, default is the timeout set in the constructor or 3000
+     * @returns {Promise<value>}
+     */
+    fetchClientValue(value, options = {}) {
+        const wrapper = this;
+        return new Promise(async(resolve, reject) => {
+            unirest.get(`${wrapper.url}/getClientData/${value}`)
+                .header(`Authorization`, wrapper.token)
+                .timeout(options.timeout ? options.timeout : wrapper.timeout)
+                .end(response => {
+                    if (typeof response.error === "object" && response.error.Error === "ETIMEDOUT") return reject("timeout");
+                    if (response.statusCode !== 200) return reject(response.body);
+                    resolve(response.body);
+                });
+        });
+    }
+
+    /**
+     * Restricted to private tokens: Create or update a user entry in the database, returns true if the operation is a success
+     * @param {Object} user The user object, the object must be valid otherwise the API will reject the request
+     * @param {Object} [options] An optional object of options
+     * @param {number} [options.timeout=this.timeout] Time in milliseconds before the request should abort, default is the timeout set in the constructor or 3000
+     * @returns {Promise<boolean||array>}
+     */
+    postUser(user, options = {}) {
+        const wrapper = this;
+        return new Promise(async(resolve, reject) => {
+            unirest.post(`${wrapper.url}/postUserData`)
+                .header(`Authorization`, wrapper.token)
+                .header(`Content-Type`, `application/json`)
+                .timeout(options.timeout ? options.timeout : wrapper.timeout)
+                .send(JSON.stringify(user))
+                .end(response => {
+                    if (typeof response.error === "object" && response.error.Error === "ETIMEDOUT") return reject("timeout");
+                    if (response.statusCode !== 200 || Array.isArray(response.body)) return reject(response.body);
+                    resolve(response.body);
+                });
+        });
+    }
+
+    /**
+     * Restricted to private tokens: Create or update a guild entry in the database, returns true if the operation is a success
+     * @param {Object} guild The guild object, the object must be valid otherwise the API will reject the request
+     * @param {Object} [options] An optional object of options
+     * @param {number} [options.timeout=this.timeout] Time in milliseconds before the request should abort, default is the timeout set in the constructor or 3000
+     * @returns {Promise<boolean||array>}
+     */
+    postGuild(guild, options = {}) {
+        const wrapper = this;
+        return new Promise(async(resolve, reject) => {
+            unirest.post(`${wrapper.url}/postGuildData`)
+                .header(`Authorization`, wrapper.token)
+                .header(`Content-Type`, `application/json`)
+                .timeout(options.timeout ? options.timeout : wrapper.timeout)
+                .send(JSON.stringify(guild))
+                .end(response => {
+                    if (typeof response.error === "object" && response.error.Error === "ETIMEDOUT") return reject("timeout");
+                    if (response.statusCode !== 200 || Array.isArray(response.body)) return reject(response.body);
+                    resolve(response.body);
+                });
+        });
+    }
+}
+
+module.exports = FelixWrapper;
